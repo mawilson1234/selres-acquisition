465f0b2ae9ecfb0ddbddc96baaf997b906256fa8
mawilson1234 <michael.a.wilson@yale.edu>
2022-06-15 23:56:00 -0400

    fix tokenizer in topk args for minibertas

--- selres.py
+++ selres.py
@@ -13,7 +13,7 @@ OmegaConf.register_new_resolver(
 
 OmegaConf.register_new_resolver(
 	'which_args',
-	lambda string_id, which_args: which_args if not which_args == 'model' else string_id.replace('google/', '').replace('-seed', '')
+	lambda string_id, which_args: which_args if not which_args == 'model' else string_id.replace('google/', '').replace('-seed', '').replace('nyu-mll/', '').replace('-base-', '_')
 )
 
 @hydra.main(config_path='conf', config_name='selres')
